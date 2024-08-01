d3a5718a2ad5424fc77c93f5873e26dcb94af372
triptibahire <triptibahire99@gmal.com>
2024-08-01 17:07:16 +0530

    Uploaded Files

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useContext } from 'react'
+import { Sidebar } from './components/Sidebar'
+import Player from './components/Player'
+import Display from './components/Display'
+import { PlayerContext } from './context/PlayerContext'
+
+const App = () => {
+
+  const { audioRef, track } = useContext(PlayerContext);
+  return (
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex' >
+        {/* <div className='w-[100%] flex' ></div> */}
+        <Sidebar />
+        <Display />
+      </div>
+      <Player />
+      <audio ref={audioRef} src={track.file} preload='auto'></audio>
+
+    </div>
+
+
+
+  )
+}
+
+export default App
